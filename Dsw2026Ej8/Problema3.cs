4b0d5d1c533dd88cbb095c54ab4aeb840fe9abcc
CarlosSuarezLemoine <aar.suarez.lemoine@gmail.com>
2026-05-02 04:35:44 -0300

    Problema 3 completado

---
+++ Dsw2026Ej8/Problema3.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Problema3
+    {
+       
+        public string CompararCopias(int originalValue, Product product)
+        {
+            int copia = originalValue;
+            copia++;
+            Product productoCopia = product;
+            productoCopia.SetDescripcionProducto("Descripcion Modificada");
+
+
+            return originalValue + "-" + copia + "-" + product.GetDescripcionProducto();
+        }
+
+
+    }
+
+    internal class Product
+    {
+        public int valorCopia;
+        public string descripcionProducto;
+
+        public int GetValorCopia()
+        {
+            return valorCopia;
+        }
+
+        public void SetValorCopia(int value)
+        {
+            valorCopia = value;
+        }
+
+        public string GetDescripcionProducto()
+        {
+            return descripcionProducto;
+        }
+
+        public void SetDescripcionProducto(string value)
+        {
+            descripcionProducto = value;
+        }
+
+    }
+}
--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -8,8 +8,16 @@ namespace Dsw2026Ej8
         {
             //ProductHelper p = new ProductHelper();
             //Console.WriteLine(p.ObtenerEtiquetaProducto(123,"Agua",1500));
-            Problema2 p2 = new Problema2();
-            Console.WriteLine(p2.CrearResumenVenta(123, "Agua", 2, 1500));
+
+            //Problema2 p2 = new Problema2();
+            //Console.WriteLine(p2.CrearResumenVenta(123, "Agua", 2, 1500));
+
+            //Problema3 p3 = new Problema3();
+            //Product prod = new Product();
+            //prod.SetDescripcionProducto("Descripcion Original");
+            //Console.Write(p3.CompararCopias(5, prod));
+
+
         }
     }
 }

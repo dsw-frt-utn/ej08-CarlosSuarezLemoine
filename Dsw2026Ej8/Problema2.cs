8606e4124b723fe42f588ad18dace51a76eb5aad
CarlosSuarezLemoine <aar.suarez.lemoine@gmail.com>
2026-05-02 04:18:14 -0300

    Problema 2 completado

---
+++ Dsw2026Ej8/Problema2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Problema2
+    {
+
+        public string CrearResumenVenta(long productCode, string productDescription, int quantity, decimal unitPrice)
+        {
+            decimal total;
+
+            if (quantity <= 0)
+            {
+                total = 0;
+            }
+            else 
+            { 
+                total = quantity * unitPrice; 
+            }
+
+           var Venta = new { code = productCode, description = productDescription,quantity = quantity,total = total};
+
+            return Venta.code + " - " + Venta.description + " - " + Venta.total;
+        }
+    }
+}
--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -7,7 +7,9 @@ namespace Dsw2026Ej8
         static void Main(string[] args)
         {
             //ProductHelper p = new ProductHelper();
-            //Console.WriteLine(p.ObtenerEtiquetaProducto(123,"Agua",1500));         
+            //Console.WriteLine(p.ObtenerEtiquetaProducto(123,"Agua",1500));
+            Problema2 p2 = new Problema2();
+            Console.WriteLine(p2.CrearResumenVenta(123, "Agua", 2, 1500));
         }
     }
 }
